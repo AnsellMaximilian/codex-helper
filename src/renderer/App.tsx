ce6d8dd09cebf041eeb7a163c935539740efd3a1
Ansell Maximilian <ansellmaximilian@gmail.com>
2025-09-24 15:25:48 +0700

    shadcn

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,16 +1,18 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
     <div className="">
       <h1 className="text-xl font-bold">Welcome</h1>
-      <button
-        className="bg-red-600 text-white"
+      <Button
+        className=""
         onClick={async () => {
           const res = await api.files.pickFolder();
           console.log(res);
         }}
       >
         Choose Project Folder
-      </button>
+      </Button>
     </div>
   );
 }
